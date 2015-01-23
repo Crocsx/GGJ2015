3d34d247e15a7b34e67edbba697d5b50fe223cbf
Ttouaty <t.touaty@student.isartdigital.com>
2015-01-24 00:10:58 +0100

    PARALAX DONE

---
+++ Assets/moveCameraTEST.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCameraTEST : MonoBehaviour {
+	public float speed = 10;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(speed * transform.right * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(speed * -transform.right * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/movePlane.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlane : MonoBehaviour {
+	
+	public float speed = 1;
+	
+	private float TextureOffset = 0;
+	private Vector3 offsetFromCamera;
+	private Vector3 newOffsetFromCamera;
+	
+
+	void Start () {
+	}
+	
+	void Update () {
+		newOffsetFromCamera = offsetFromCamera - (transform.position - Camera.main.transform.position);
+		TextureOffset += newOffsetFromCamera.x * speed * 0.005f;
+		transform.position = new Vector3(Camera.main.transform.position.x,Camera.main.transform.position.y,transform.position.z);
+		renderer.material.mainTextureOffset = new Vector2(TextureOffset,0);
+		offsetFromCamera = transform.position - Camera.main.transform.position;
+	}
+}
