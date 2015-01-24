9451a06b7814d360b9226ac240410041c56e2199
Crocsx <crocsx.gaming@gmail.com>
2015-01-24 04:03:25 +0100

    prefabs

---
+++ Assets/Script/Enemy/Shark.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shark : pEnemy {
+
+    public int _distanceParcour;
+    public float _timeForMove;
+    public bool _side;
+
+    private Vector3 _posMaxLeft;
+    private Vector3 _posMaxRight;
+    private Vector3 _posTarget;
+    private Vector3 _posActual;
+    private float counter;
+	// Use this for initialization
+	void Start () {
+        base.Start();
+        _posMaxLeft = _transform.position - new Vector3(_distanceParcour * 0.5f, 0, 0);
+        _posMaxRight = _transform.position + new Vector3(_distanceParcour * 0.5f, 0,0);
+        _posTarget = _side ? _posMaxLeft : _posMaxRight;
+        _posActual = _transform.position;
+        counter = _distanceParcour*0.5f; 
+	}
+
+    public override void Effect()
+    {
+        // one Shoot
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime;
+        Vector3 newMove = Vector3.Lerp(_posActual, _posTarget, counter/_timeForMove);
+        Debug.Log(newMove);
+        _transform.position = newMove;
+
+        if (_side && _transform.position.x <= _posTarget.x)
+            SwitchSide();
+        else if (!_side && _transform.position.x >= _posTarget.x)
+            SwitchSide();
+	}
+
+    void SwitchSide()
+    {
+        _side = !_side;
+        _posActual = _posTarget;
+        _posTarget = _side ? _posMaxLeft : _posMaxRight;
+        counter = 0;
+    }
+}
---
+++ Assets/Script/Enemy/pEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pEnemy : MonoBehaviour {
+
+    protected Transform _transform;
+	// Use this for initialization
+	public void Start () {
+        _transform = transform;
+	}
+
+    public virtual void Effect() { }
+
+	// Update is called once per frame
+	void Update () {
+    
+    }
+}
